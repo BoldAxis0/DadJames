8eeb7b9e6a9ea5250702258ab259fccdc08cd6be
BoldAxis0 <48401574+BoldAxis0@users.noreply.github.com>
2021-12-28 23:03:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+#a bot for dad jokes
+
+import discord
+import os
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print("User ready as: {0.user}".format(client))
+
+
+def remove_prefix(text, prefix):
+    return text[text.startswith(prefix) and len(prefix):]
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    else:
+        msg = message.content
+        triggerWords = ["I'm ", "Im ", "im ", "I am ", "i am ", "IM ", "I AM "]
+        listOfMatches = [ele in msg for ele in triggerWords]
+        if any(listOfMatches):
+            indexOfTrigger = listOfMatches.index(True)
+            wordThatTriggered = triggerWords[indexOfTrigger]
+
+            response = msg[msg.index(wordThatTriggered):]
+            response = remove_prefix(response, wordThatTriggered)
+
+            #print(response)
+
+            await message.channel.send("Hi {}, I'm DadJames".format(response))
+
+
+my_token = os.environ['TOKEN']
+client.run(my_token)
